d2f53d2310ff72e0536e1f04df7b2f3783070134
Ghionab <abrahammulualem3@gmail.com>
2025-03-20 22:19:22 +0300

    Initial Commit

---
+++ yaho.py
@@ -0,0 +1,20 @@
+import requests
+
+url="https://finance.yahoo.com/quote/MSFT/history/"
+params = {
+    "period1": 1734800036,  # Start timestamp
+    "period2": 1735231689,  # End timestamp
+    "interval": "1d",       # Daily data
+    "events": "history",
+    "includeAdjustedClose": "true"
+}
+
+headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+  }
+
+response= requests.get(url, headers=headers)
+
+with open("yaho3.csv","wb") as file:
+    file.write(response.content)
+print("200")
