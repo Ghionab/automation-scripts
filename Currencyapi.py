6a9fe043a38664054ccb721f1e92bbb5aca02ff2
Ghionab <abrahammulualem3@gmail.com>
2025-03-27 11:35:15 +0300

    Updated Format Strings

--- Currencyapi.py
+++ Currencyapi.py
@@ -2,8 +2,8 @@ from flask import Flask,jsonify
 from bs4 import BeautifulSoup
 import requests
 
-def get_c(inp, outp):
-    url = f"https://www.x-rates.com/calculator/?from={inp}&to={outp}&amount=1"
+def get_c(input, output):
+    url = f"https://www.x-rates.com/calculator/?from={input}&to={output}&amount=1"
     content=requests.get(url).text
     soup=BeautifulSoup(content, 'html.parser')
     currency=soup.find("span", class_="ccOutputRslt" ).get_text()
@@ -17,9 +17,9 @@ def home():
     return "<h1>Currenct Rate</h1>"
 
 @app.route(f"/api/<inp>-<outp>")
-def main(inp,outp):
-    rate=get_c(inp, outp)
-    rate_dictionary={"input": inp, "output": outp, "rate":rate }
+def main(input,output):
+    rate=get_c(input, output)
+    rate_dictionary={"input": input, "output": output, "rate":rate }
     return jsonify(rate_dictionary)
 
 app.run(host="0.0.0.0")
